63d2fc0e67901612b4c4c2b281da6789b177286d
Paul B <paulbg1993@gmail.com>
2021-03-07 18:13:28 +0100

    erase security for tryouts

--- edge-service/src/main/java/com/pantry/app/edgeservice/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pantry.app.edgeservice.repository;
-
-import com.pantry.app.edgeservice.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
---
+++ pantry-microserver/src/main/java/com/pantry/app/pantry/microserver/pantrymicroserver/controller/PantryController.java
@@ -0,0 +1,9 @@
+package com.pantry.app.pantry.microserver.pantrymicroserver.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PantryController {
+
+
+}
